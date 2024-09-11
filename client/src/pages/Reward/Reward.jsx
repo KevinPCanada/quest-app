6c4daf7a5ff9a01d5fbd52c6080088ba9c2fc088
StephenRossW <sross443@googlemail.com>
2024-09-11 20:16:09 +0900

    RewardsPage

--- client/src/pages/Reward/Reward.jsx
+++ client/src/pages/Reward/Reward.jsx
@@ -2,47 +2,16 @@
 import React from 'react'
 import './Reward.css'
 
+import RewardPage from '../../components/RewardPage/RewardPage'
+
 export default function Reward() {
 
-
-
+    return  <>
+    <RewardPage></RewardPage>
+           </>
+     
     
-        return (
-            <div>
-                <h1>Rewards</h1>
-                <div>
-                    <h2>Set new reward</h2>
-                    <form>
-                        <input type="text" />
-                        <input type='submit'>Add new reward</input>
-                    </form>
-                </div>
-
-                <div>
-                    <h2>See list of rewards</h2>
-                    <p>Coffee</p>
-                    <p>New watch</p>
-                    <p>Fish and chips</p>
-                </div>
-
-                <div>
-                    <h2>
-                        Set milestone
-                    </h2>
-                    <form>
-                        <p>How often would you like to set your milestone?</p>
-                        <input name='milestone' type='radio'>Every level</input>
-                        <input name='milestone' type='radio'>Every 2 levels</input>
-                        <input name='milestone' type='radio'>Every 5 levels</input>
-                        <input name='milestone' type='radio'>Every 10 levels</input>
-                    </form>
-
-                </div>
-            </div>
-
-
-        )
-    }
+}
 
 
 
