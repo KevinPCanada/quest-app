b5e499c7f9f282f745d3f190e29e6520c7e170f5
StephenRossW <sross443@googlemail.com>
2024-09-12 22:01:01 +0900

    Quick edit

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import LevelBar from '../../components/Level/Level'
-import FullQuest from '../../components/Full_Quest/Full_Quest'
+
 
 function Home() {
   return (
@@ -8,7 +8,7 @@ function Home() {
       <h1>Home Page</h1>
       <LevelBar exp='143255' ></LevelBar>
       <p>Welcome to the home page!</p>
-      <FullQuest Quest={{name:'Do homework', description:'Javascript homework for w-coding', questLevel:'Challenging'}}></FullQuest>
+      
     </div>
 
   )
