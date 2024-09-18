2cd7f911213410d4230a290e2db14b739f6ab5a9
itzguchi <ayumuiguchi94@gmail.com>
2024-09-18 20:40:38 +0900

    tweaked app.jsx. finished home jsx and fixed sidebar jsx

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Sidebar.css'
 
 //This is the sidebar component,that will be accessible from any page and allow the user to navigate the webapp easily
 
@@ -7,21 +8,21 @@ export function Sidebar() {
     <div>
        <aside className="sidebar">
             <a href="" className="profile">
-                <span className="material-symbols-outlined">account_circle</span>
+                <i className="material-icons">account_circle</i>
                 <h1>user_name</h1>
             </a>
             <nav className="sidebar-menu">
                 <ul>
                     <a className="sidebar-button" href="">
-                        <span className="material-symbols-outlined">star</span>
+                        <i className="material-icons">star</i>
                         <p>Rewards</p>
                     </a>
                     <a className="sidebar-button" href="">
-                        <span className="material-symbols-outlined">receipt_long</span>
+                        <i className="material-icons">receipt_long</i>
                         <p>Questboard</p>
                     </a>
                     <a className="sidebar-button" href="">
-                        <span className="material-symbols-outlined">menu_book</span>
+                        <i className="material-icons">menu_book</i>
                         <p>Quest History</p>
                     </a>
                 </ul>
@@ -31,7 +32,7 @@ export function Sidebar() {
                 <a href="">Privacy Policy</a>
             </nav>
             <a href="" className="logout">
-                <span className="material-symbols-outlined">settings</span>
+                <i className="material-icons">settings</i>
                 <p>Logout</p>
             </a>
         </aside>
