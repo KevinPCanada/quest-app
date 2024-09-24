1a489fd2fccbdc5e3d48ce1f1e99cbe48db3a520
StephenRossW <sross443@googlemail.com>
2024-09-24 21:47:18 +0900

    Controllers for quests

--- api/routes/quests.js
+++ api/routes/quests.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import mysql from 'mysql2';
-import { addQuest, deleteQuest, modifyQuest, displayAllQuests, getQuestByID, completeQuest } from '../controllers/quests.js';
+import multer from 'multer';
+import { addQuest, deleteQuest, modifyQuest, displayAllQuests, getQuestByID, completeQuest, getCompletedQuests, getIncompleteQuests, getExpByQuest } from '../controllers/quests.js';
 
 const app = express();
 // const port = 8800;
 const router = express.Router()
+
+const upload = multer()
 
 
 const db = mysql.createConnection({
@@ -25,12 +28,25 @@ db.connect(err => {
 
 // Define the route to get tasks for a specific user
 
-router.post('/add-quest', addQuest)
-router.delete('/delete-quest/:id', deleteQuest)
-router.put('/modify-quest', modifyQuest)
-router.get('/display-user/:userId', displayAllQuests)
-router.get('/display-quest/:questID', getQuestByID)
-router.put('/complete-quest/:questID', completeQuest)
+//add/delete a quest
+router.post('/add-quest', upload.none(), addQuest)
+router.delete('/delete-quest/:questId', deleteQuest)
+//modify the name/description/level of a quest
+router.put('/modify-quest', upload.none(), modifyQuest)
+//display all of the quest of one user
+router.get('/display-user/:userId', upload.none(), displayAllQuests)
+//display a specific quest based on it's ID
+router.get('/display-quest/:questId', upload.none(), getQuestByID)
+//mark a quest as complete
+router.put('/complete-quest/:questId', completeQuest)
+//get a list of all of one user's completed quests
+router.get('/completed/:userId', getCompletedQuests)
+//get a list of all of one user's incomplete quests
+router.get('/incomplete/:userId', getIncompleteQuests)
+//get a quests's experience by the quest ID
+router.get('/exp/:questId', getExpByQuest)
+
+
 
 
 
