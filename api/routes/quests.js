f0c2384eb0db8974c152737aa77c41b7c8544e99
StephenRossW <sross443@googlemail.com>
2024-09-24 21:57:58 +0900

    Comments

--- api/routes/quests.js
+++ api/routes/quests.js
@@ -28,25 +28,46 @@ db.connect(err => {
 
 // Define the route to get tasks for a specific user
 
-//add/delete a quest
+
+// Define the route to get tasks for a specific user
+
+//add a quest. 
+//example query POST http://localhost:8800/api/quests/add-quest
+//body questName: Shopping questDescription: Grocery shopping questLevel: 1
 router.post('/add-quest', upload.none(), addQuest)
+
+//delete a quest
+//example query DELETE http://localhost:8800/api/quests/delete-quest/2
 router.delete('/delete-quest/:questId', deleteQuest)
+
 //modify the name/description/level of a quest
+//example quest PUT http://localhost:8800/api/quests/modify-quest
+//body questName: Shopping questDescription: Grocery shopping questLevel: 1 questId: 6
 router.put('/modify-quest', upload.none(), modifyQuest)
+
 //display all of the quest of one user
+// example query GET http://localhost:8800/api/quests/display-user/2
 router.get('/display-user/:userId', upload.none(), displayAllQuests)
+
 //display a specific quest based on it's ID
+//example query GET http://localhost:8800/api/quests/display-quest/4
 router.get('/display-quest/:questId', upload.none(), getQuestByID)
+
 //mark a quest as complete
+// example query PUT http://localhost:8800/api/quests/complete-quest/3
 router.put('/complete-quest/:questId', completeQuest)
+
 //get a list of all of one user's completed quests
+//example query GET http://localhost:8800/api/quests/completed/2
 router.get('/completed/:userId', getCompletedQuests)
+
 //get a list of all of one user's incomplete quests
+//example query GET http://localhost:8800/api/quests/incomplete/2
 router.get('/incomplete/:userId', getIncompleteQuests)
+
 //get a quests's experience by the quest ID
+//example query GET http://localhost:8800/api/quests/exp/7
 router.get('/exp/:questId', getExpByQuest)
-
-
 
 
 
