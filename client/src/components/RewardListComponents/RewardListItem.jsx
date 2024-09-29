d124ec098bdd57fb6c907ca85ea8b1c939c103d4
KevinPCanada <kevinpcanada@gmail.com>
2024-09-29 15:41:40 +0900

    Add the edit-reward FE component and made a few style changes.

--- client/src/components/RewardListComponents/RewardListItem.jsx
+++ client/src/components/RewardListComponents/RewardListItem.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import ModifyRewardsButton from "./ModifyRewardsButton";
 import "./RewardListItem.css"
 
 export default function RewardListItem({ reward }) {
@@ -9,7 +10,7 @@ export default function RewardListItem({ reward }) {
         <p>{reward}</p>
       </div>
       <div className="reward-actions">
-        <input type="button" value="Modify" className="eightbit-btn modify-btn" />
+        <ModifyRewardsButton/>
         <input type="button" value="Delete" className="eightbit-btn delete-btn" />
       </div>
     </div>
