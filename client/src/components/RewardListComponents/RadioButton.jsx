fec4de49cde8bbb35f76e7ad3258a0f6531e60e4
KevinPCanada <kevinpcanada@gmail.com>
2024-10-01 01:14:05 +0900

    Radio Button Change

--- client/src/components/RewardListComponents/RadioButton.jsx
+++ client/src/components/RewardListComponents/RadioButton.jsx
@@ -2,10 +2,12 @@ import React from "react"
 import "./RadioButton.css"
 
 export default function RewardRadio({ label }) {
+  const id = `radio-${label}`; // Unique id for each radio input
+
   return (
     <div className="reward-radio">
-      <p>{label}</p>
-      <input name="milestone" type="radio" />
+      <label htmlFor={id}>{label}</label>
+      <input id={id} name="milestone" type="radio" />
     </div>
   )
 }
